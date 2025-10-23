be6c6017144be471257b019592fffd29858cbcb7
Sharlotte <nguyennhonhau25052003@gmail.com>
2025-10-24 00:03:35 +0700

    feat(ServerMetadata): add required JsonProperty annotation to config field
    Add @JsonProperty(required=true) to ensure config field is mandatory in JSON serialization

--- src/main/java/mindustrytool/servermanager/types/data/ServerMetadata.java
+++ src/main/java/mindustrytool/servermanager/types/data/ServerMetadata.java
@@ -1,5 +1,7 @@
 package mindustrytool.servermanager.types.data;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
@@ -9,5 +11,7 @@ import lombok.experimental.Accessors;
 @Accessors(chain = true)
 public class ServerMetadata {
     private String serverImageHash;
+
+    @JsonProperty(required = true)
     private ServerConfig config;
 }
