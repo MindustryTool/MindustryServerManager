6d986438b647f1db3a69d3dc04b2854ae0935938
Sharlotte <nguyennhonhau25052003@gmail.com>
2025-10-28 20:29:42 +0700

    perf(HttpServer): increase state update interval from 15 to 45 seconds
    Reduce frequency of state updates to lower system load while maintaining functionality

--- plugin/src/main/java/plugin/handler/HttpServer.java
+++ plugin/src/main/java/plugin/handler/HttpServer.java
@@ -100,7 +100,7 @@ public class HttpServer {
     public static void init() {
         ServerController.BACKGROUND_SCHEDULER.scheduleWithFixedDelay(() -> {
             sendStateUpdate();
-        }, 0, 15, TimeUnit.SECONDS);
+        }, 0, 45, TimeUnit.SECONDS);
 
         app = Javalin.create(config -> {
             config.showJavalinBanner = false;
