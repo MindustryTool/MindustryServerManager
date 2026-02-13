dd9998ece5af7194d3144c7041b985d127bccf31
Sharlotte <nguyennhonhau25052003@gmail.com>
2026-02-13 23:04:04 +0700

    refactor(rank): remove unused session creation listener
    The onSessionCreated listener was removed as it was unused and causing unnecessary IO tasks. This simplifies the RankService by removing dependencies on Tasks, SessionRepository, and RankUtils that are no longer needed.

--- plugin/src/main/java/plugin/service/RankService.java
+++ plugin/src/main/java/plugin/service/RankService.java
@@ -3,30 +3,17 @@ package plugin.service;
 import lombok.RequiredArgsConstructor;
 import mindustry.gen.TimedKillc;
 import plugin.Cfg;
-import plugin.Tasks;
 import plugin.annotations.Component;
 import plugin.annotations.ConditionOn;
 import plugin.annotations.Listener;
 import plugin.event.PlayerKillUnitEvent;
-import plugin.event.SessionCreatedEvent;
-import plugin.repository.SessionRepository;
-import plugin.utils.RankUtils;
 
 @Component
 @ConditionOn(Cfg.OnOfficial.class)
 @RequiredArgsConstructor
 public class RankService {
-    private final SessionRepository sessionRepository;
     private final SessionHandler sessionHandler;
     private final SessionService sessionService;
-
-    @Listener
-    private void onSessionCreated(SessionCreatedEvent event) {
-        Tasks.io("Send Leader Board",
-                () -> event.session.player
-                        .sendMessage(RankUtils.getRankString(event.session.locale, sessionRepository.leaderBoard(10))));
-
-    }
 
     @Listener
     public void onPlayerKillUnit(PlayerKillUnitEvent event) {
