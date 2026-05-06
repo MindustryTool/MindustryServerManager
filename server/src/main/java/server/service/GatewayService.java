12ff5589638c268ece4b80c5c3f1ed1bc73a9a66
Sharlotte <nguyennhonhau25052003@gmail.com>
2026-05-06 18:19:19 +0700

    fix(gateway): include gateway ID in connection error message
    Include the gateway identifier in the error message to aid debugging when connection attempts fail.

--- server/src/main/java/server/service/GatewayService.java
+++ server/src/main/java/server/service/GatewayService.java
@@ -267,7 +267,7 @@ public class GatewayService {
                     try {
                         connectedFuture.get(1, TimeUnit.MINUTES);
                     } catch (InterruptedException | ExecutionException | TimeoutException e) {
-                        return CompletableFuture.failedFuture(new RuntimeException("Server gateway not connected", e));
+                        return CompletableFuture.failedFuture(new RuntimeException("Server gateway not connected: " + id, e));
                     }
                 }
 
